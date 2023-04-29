c227b7c7ee408868ae9ca41437d44ff712c35fa6
monterourena <davidmonterourena@gmail.com>
2023-04-29 08:09:10 -0600

    default port configuration

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 import express from "express";
 // CONFIGURATION
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
